992e561b5a7cb9c20e8b51907a4cc69adbdb9f1e
Maxim Voynov <virus14m@gmail.com>
2019-04-17 11:19:04 +0700

    Logic added

--- app/src/main/java/com/example/beeradviser/MainActivity.java
+++ app/src/main/java/com/example/beeradviser/MainActivity.java
@@ -1,22 +1,40 @@
 package com.example.beeradviser;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClickFindBeer(View view){
+
+    //Вызывается при щелчке на кнопке
+    public void onClickFindBeer(View view) {
+        //Получить ссылку на TextView
         TextView brands = (TextView) findViewById(R.id.brands);
+        //Получить ссылку на Spinner
         Spinner color = (Spinner) findViewById(R.id.color);
-        String valueOf(color.getSelectedItem())
+        //Получить вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Получить контейнер List с сортами пива.
+        List<String> brandsList = expert.getBrands(beerType);
+        //Построить String по данным из List.
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            //Каждый сорт выводится с новой строки.
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Вывести сорта пива
+        //Вывести результаты в надписи.
+        brands.setText(brandsFormatted);
     }
 }
